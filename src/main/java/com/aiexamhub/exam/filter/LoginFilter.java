8eb5fa19e7f087e03da154f1d96df2adbeacd926
cbmja <cbmja@naver.com>
2025-03-04 13:30:53 +0900

    .로그인 필터 수정

--- src/main/java/com/aiexamhub/exam/filter/LoginFilter.java
+++ src/main/java/com/aiexamhub/exam/filter/LoginFilter.java
@@ -9,6 +9,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
+import java.util.Set;
+import java.util.HashSet;
 
 @RequiredArgsConstructor
 @Component
@@ -23,32 +25,54 @@ public class LoginFilter implements Filter {
         HttpServletRequest req = (HttpServletRequest) servletRequest;
 
         String reqUri = req.getRequestURI();
+        System.out.println(reqUri+"==============================================");
+
+        if (reqUri.endsWith(".js") || reqUri.endsWith(".css")) {
+            System.out.println("1==============================================");
+            filterChain.doFilter(servletRequest, servletResponse);
+            return;
+        }
 
         Cookie[] cookies = req.getCookies();
         String idCookie = "";
         if (cookies != null) {
             for (Cookie cookie : cookies) {
                 if ("mCode".equals(cookie.getName())) {
-                    idCookie = cookie.getValue(); // 원하는 쿠키 반환
+                    idCookie = cookie.getValue();
+                    break;
                 }
             }
         }
 
         Boolean isLogin = !idCookie.isEmpty();
-        servletRequest.setAttribute("isLogin" , isLogin);
+        servletRequest.setAttribute("isLogin", isLogin);
 
-        if(!idCookie.isEmpty()){
+        if (isLogin) {
             idCookie = cipherUtil.decrypt(idCookie);
         }
+        servletRequest.setAttribute("memberCode", idCookie);
 
-        servletRequest.setAttribute("memberCode" , idCookie);
+        // 로그인 없이 접근 가능한 API 목록
+        Set<String> logOutPath = new HashSet<>();
+        logOutPath.add("/index");
+        logOutPath.add("/member/join");
+        logOutPath.add("/member/login");
+        logOutPath.add("/member/logout");
 
+        // 로그아웃 상태에서 접근 가능한 경로 확인
+        if (logOutPath.contains(reqUri)) {
+            System.out.println("2==============================================");
+            filterChain.doFilter(servletRequest, servletResponse);
+            return;
+        }
 
-
-
-
+        // 로그인 상태가 아니라면 `/index`로 리다이렉트
+        if (!isLogin) {
+            System.out.println("3==============================================");
+            httpResponse.sendRedirect("/index");
+            return;
+        }
+        System.out.println("4==============================================");
         filterChain.doFilter(servletRequest, servletResponse);
-
-
     }
 }
